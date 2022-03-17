86e3ab67389cb925cd9dfb084ff5857fd674329e
mashru020 <mashrurrahman16@gmail.com>
2022-03-17 12:41:32 +0600

    on click event finger color change added

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log("hello");
+const hand = document.getElementById('hand');
+
+hand.addEventListener('click',function(e){
+    console.log('clicked on hand');
+    console.log(e);
+    const id = e.target.getAttribute('alt');
+    console.log(id);
+    document.querySelector(`.${id}`).style.opacity=1;
+ });
